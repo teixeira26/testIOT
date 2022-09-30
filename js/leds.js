4c54c033d5f647aeb18d7617d1e785f765fd450e
shift <teixeira.matheus2626@gmail.com>
2022-09-29 23:15:34 -0300

    (index.html)-agregado meta que permite peticiones mixtas

--- js/leds.js
+++ js/leds.js
@@ -1,12 +1,16 @@
 const prenderLed=()=>{
-    fetch('http://192.168.0.5/LED=ON')
+    fetch('http://192.168.0.5/LED=ON',{
+        referrerPolicy: "unsafe_url" 
+    })
     .then(x=>console.log(x))
     .catch(error=>console.log(error))
 }
 
 
 const apagarLed = ()=>{
-    fetch('http://192.168.0.5/LED=OFF')
+    fetch('http://192.168.0.5/LED=OFF',{
+        referrerPolicy: "unsafe_url" 
+    })
     .then(x=>console.log(x))
     .catch(error=>console.log(error))
 }
