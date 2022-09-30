f0cdc514ae3ab628f879ea17047d50c242206b1b
shift <teixeira.matheus2626@gmail.com>
2022-09-29 23:19:23 -0300

    (index.html) - cambiado el valor de referrer policy en fetch

--- js/leds.js
+++ js/leds.js
@@ -1,6 +1,6 @@
 const prenderLed=()=>{
     fetch('http://192.168.0.5/LED=ON',{
-        referrerPolicy: "unsafe_url" 
+        referrerPolicy: "unsafe-url" 
     })
     .then(x=>console.log(x))
     .catch(error=>console.log(error))
@@ -9,7 +9,7 @@ const prenderLed=()=>{
 
 const apagarLed = ()=>{
     fetch('http://192.168.0.5/LED=OFF',{
-        referrerPolicy: "unsafe_url" 
+        referrerPolicy: "unsafe-url" 
     })
     .then(x=>console.log(x))
     .catch(error=>console.log(error))
